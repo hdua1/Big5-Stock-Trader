4c7aa10ec913923016c93d11a4802e4edb3c339c
hdua1@asu.edu <hdua1@asu.edu>
2018-07-09 00:05:13 -0700

    adding files

---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <div>
+        <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+    </div>
+</div>
+        </div>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+    import Stock from './Stock.vue';
+
+    export default {
+        computed: {
+            ...mapGetters({
+                stocks: 'stockPortfolio'
+            })
+        },
+        components: {
+            appStock: Stock
+        }
+    }
+</script>
---
+++ src/data/stocks.js
@@ -0,0 +1,7 @@
+export default [
+    { id: 1, name: 'Facebook', price: 110 },
+    { id: 2, name: 'Google', price: 348 },
+    { id: 3, name: 'Microsoft', price: 564 },
+    { id: 4, name: 'Apple', price: 123 },
+    { id: 5, name: 'Amazon', price: 258 }
+];
